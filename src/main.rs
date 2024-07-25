dca9b10833e0b3771cb98ff7c670e8a1ec23ec37
EscasanN <escalantesanchez3@gmail.com>
2024-07-24 23:28:45 -0600

    Finished the last polygon: the teapot.

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,62 @@ fn main() {
         Vec3::new(436.0, 249.0, 0.0)
     ];
 
+    let poly5 = vec![
+        (413, 177),
+        (448, 159),
+        (502, 88),
+        (553, 53),
+        (535, 36),
+        (676, 37),
+        (660, 52),
+        (750, 145),
+        (761, 179),
+        (672, 192),
+        (659, 214),
+        (615, 214),
+        (632, 230),
+        (580, 230),
+        (597, 215),
+        (552, 214),
+        (517, 144),
+        (466, 180)
+    ];
+
+    let poly5_f = vec![
+        Vec3::new(413.0, 177.0, 0.0),
+        Vec3::new(448.0, 159.0, 0.0),
+        Vec3::new(502.0, 88.0, 0.0),
+        Vec3::new(553.0, 53.0, 0.0),
+        Vec3::new(535.0, 36.0, 0.0),
+        Vec3::new(676.0, 37.0, 0.0),
+        Vec3::new(660.0, 52.0, 0.0),
+        Vec3::new(750.0, 145.0, 0.0),
+        Vec3::new(761.0, 179.0, 0.0),
+        Vec3::new(672.0, 192.0, 0.0),
+        Vec3::new(659.0, 214.0, 0.0),
+        Vec3::new(615.0, 214.0, 0.0),
+        Vec3::new(632.0, 230.0, 0.0),
+        Vec3::new(580.0, 230.0, 0.0),
+        Vec3::new(597.0, 215.0, 0.0),
+        Vec3::new(552.0, 214.0, 0.0),
+        Vec3::new(517.0, 144.0, 0.0),
+        Vec3::new(466.0, 180.0, 0.0)
+    ];
+
+    let poly6 = vec![
+        (682, 175),
+        (708, 120),
+        (735, 148),
+        (739, 170)
+    ];
+
+    let poly6_f = vec![
+        Vec3::new(682.0, 175.0, 0.0),
+        Vec3::new(708.0, 120.0, 0.0),
+        Vec3::new(735.0, 148.0, 0.0),
+        Vec3::new(739.0, 170.0, 0.0)
+    ];
+        
     framebuffer.set_current_color(0xEAE52D);
 
     framebuffer.filled_polygon(&poly2);
@@ -84,6 +140,15 @@ fn main() {
     framebuffer.filled_polygon(&poly4_f);
     framebuffer.set_current_color(0xFFFFFF);
     framebuffer.polygon(&poly4);
+
+    framebuffer.set_current_color(0x04941F);
+    framebuffer.filled_polygon(&poly5_f);
+    framebuffer.set_current_color(0xFFFFFF);
+    framebuffer.polygon(&poly5);
+    framebuffer.set_current_color(0x000000);
+    framebuffer.filled_polygon(&poly6_f);
+    framebuffer.set_current_color(0xFFFFFF);
+    framebuffer.polygon(&poly6);
 
     let _ = framebuffer.render_buffer("output.bmp");
 
